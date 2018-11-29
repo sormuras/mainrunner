af2ece69d078c06034ac6748e70badfeeccfc56c
Christian Stein <sormuras@gmail.com>
2018-11-29 11:21:07 +0100

    Introduce mainrunner-engine-base-8 module

---
+++ mainrunner-engine-base-8/src/main/java/de/sormuras/mainrunner/engine/MainrunnerTestEngine.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.mainrunner.engine;
+
+import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.EngineExecutionListener;
+import org.junit.platform.engine.ExecutionRequest;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.TestEngine;
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.support.descriptor.EngineDescriptor;
+
+/** Main-invoking TestEngine implementation. */
+public class MainrunnerTestEngine implements TestEngine {
+
+  static String ENGINE_ID = "mainrunner";
+
+  static String ENGINE_DISPLAY_NAME = "Mainrunner";
+
+  @Override
+  public String getId() {
+    return ENGINE_ID;
+  }
+
+  @Override
+  public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
+    return new EngineDescriptor(uniqueId, ENGINE_DISPLAY_NAME);
+  }
+
+  @Override
+  public void execute(ExecutionRequest request) {
+    TestDescriptor engine = request.getRootTestDescriptor();
+    EngineExecutionListener listener = request.getEngineExecutionListener();
+    listener.executionStarted(engine);
+    listener.executionFinished(engine, TestExecutionResult.successful());
+  }
+}
