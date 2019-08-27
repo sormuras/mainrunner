564ec1bf44a64116024dda858c948328441b8d68
Christian Stein <sormuras@gmail.com>
2019-08-27 15:02:49 +0200

    Add integration tests and programs

---
+++ src/integration/test/java/module-info.java
@@ -0,0 +1,8 @@
+open module integration {
+  requires programs;
+  requires org.junit.jupiter.api;
+
+  requires org.junit.platform.console;
+  requires org.junit.platform.testkit;
+  requires org.assertj.core;
+}
