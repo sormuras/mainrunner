27e4689f3d6c81a03d00d004d712e8e074d64c1e
Christian Stein <sormuras@gmail.com>
2019-09-30 05:47:42 +0200

    Provide test launcher in integration module

--- src/integration/test/java/module-info.java
+++ src/integration/test/java/module-info.java
@@ -1,8 +1,10 @@
 open module integration {
   requires programs;
   requires org.junit.jupiter;
-
   requires org.junit.platform.console;
   requires org.junit.platform.testkit;
   requires org.assertj.core;
+
+  provides java.util.spi.ToolProvider with
+      integration.ModularTestLauncher;
 }
