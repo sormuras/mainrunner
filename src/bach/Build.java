5f2a74c7056abd72e648a4a49680bb35458c5b3b
Christian Stein <sormuras@gmail.com>
2019-12-23 16:30:00 +0100

    Upgrade build program to Bach.java 2.0-RC1

--- src/bach/Build.java
+++ src/bach/Build.java
@@ -1,16 +1,9 @@
 import de.sormuras.bach.Bach;
-import de.sormuras.bach.Log;
 import de.sormuras.bach.project.Configuration;
 
 class Build {
   public static void main(String[] args) {
     // Bach.build("mainrunner", "2.1.6");
-    Bach.build(
-        Log.ofSystem(),
-        Configuration.of("mainrunner", "2.1.6")
-            .setDeployment(
-                "de.sormuras.mainrunner",
-                "bintray-sormuras-maven",
-                "https://api.bintray.com/maven/sormuras/maven/mainrunner/;publish=1"));
+    Bach.build(Configuration.of("mainrunner", "2.1.6").setGroup("de.sormuras.mainrunner"));
   }
 }
