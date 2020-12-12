be0aba8615780c5fda053daf68eb7c2480b60968
Christian Stein <sormuras@gmail.com>
2020-12-12 12:10:38 +0100

    Upgrade to Bach 16-ea

--- integration/test/java/module-info.java
+++ integration/test/java/module-info.java
@@ -3,6 +3,8 @@ open module integration {
 
   requires programs;
   requires org.junit.jupiter;
+  requires org.junit.platform.console;
+  requires org.junit.platform.launcher;
   requires org.junit.platform.testkit;
   requires org.assertj.core;
 
--- src/bach/Build.java
+++
@@ -1,9 +0,0 @@
-import de.sormuras.bach.Bach;
-import de.sormuras.bach.project.Configuration;
-
-class Build {
-  public static void main(String[] args) {
-    // Bach.build("mainrunner", "2.1.6");
-    Bach.build(Configuration.of("mainrunner", "2.1.6").setGroup("de.sormuras.mainrunner"));
-  }
-}
