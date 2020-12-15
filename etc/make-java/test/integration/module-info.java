78d55f5e9ada69bde420b9fb0befc06a991d1487
Christian Stein <sormuras@gmail.com>
2020-12-15 05:46:00 +0100

    Prune deprecated configuration files

--- etc/make-java/test/integration/module-info.java
+++
@@ -1,6 +0,0 @@
-open module integration {
-  requires org.junit.jupiter.api;
-
-  requires org.junit.platform.testkit;
-  requires org.assertj.core;
-}
--- etc/maven-projects/mainrunner-engine/src/test/java/Test1.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-public class Test1 {
-  public static void main(String[] args) {
-    System.out.println("Test1.main(" + Arrays.asList(args) + ")");
-  }
-}
