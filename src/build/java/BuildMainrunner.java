aa29a057bd84ffd3f1235abbb46d056645639809
Christian Stein <sormuras@gmail.com>
2021-10-29 11:38:00 +0200

    Set version to 2.1.6

--- src/build/java/BuildMainrunner.java
+++ src/build/java/BuildMainrunner.java
@@ -101,7 +101,7 @@ public class BuildMainrunner {
         Path.of(""),
         Path.of("bin"),
         "mainrunner",
-        ModuleDescriptor.Version.parse("2.1.5"),
+        ModuleDescriptor.Version.parse("2.1.6"),
         library,
         List.of(main, test));
   }
